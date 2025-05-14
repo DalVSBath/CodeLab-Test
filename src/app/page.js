046bebc5a555267fc7e5b134d97baa08c64f4bf6
dal34 <dal34@bath.ac.uk>
2025-05-14 17:07:00 +0100

    SAML

--- src/app/page.js
+++ src/app/page.js
@@ -16,16 +16,10 @@ export default async function Home(props) {
   // Using seachParams which Next.js provides, allows the filtering to happen on the server-side, for example:
   // ?city=London&category=Indian&sort=Review
   const { firebaseServerApp } = await getAuthenticatedAppForUser();
-  const restaurants = await getRestaurants(
-    getFirestore(firebaseServerApp),
-    searchParams
-  );
+
   return (
     <main className="main__home">
-      <RestaurantListings
-        initialRestaurants={restaurants}
-        searchParams={searchParams}
-      />
+      
     </main>
   );
 }
